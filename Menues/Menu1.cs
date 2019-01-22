d3bd001f2fb6857f5db863464cf0fc80789b6383
mortenjull <mortenjull@hotmail.com>
2019-01-22 14:37:52 +0100

    Rewritten Applikation to be more streamlined

--- Menues/Menu1.cs
+++ Menues/Menu1.cs
@@ -2,56 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Inn2PowerDataStreamUpdater.BLL;
 using Inn2PowerDataStreamUpdater.Misc;
+using Inn2PowerDataStreamUpdater.Misc.Entities;
 using Inn2PowerDataStreamUpdater.Services;
 
 namespace Inn2PowerDataStreamUpdater.Menues
 {
     public class Menu1
-    {
-        private readonly AuthService _authService;
+    {       
         private readonly ApiService _apiService;
+        private readonly DataStreamService _dataStreamService;
+        private readonly Logic _logic;
+
         private readonly string _apiConnectionString;
+        private readonly string _datastreamUrl;
         private string _token;
-        private readonly string _username;
-        private readonly string _password;
 
-        private List<APICompany> _companiesToUpdate;
-        private List<APICompany> _newCompanies;
-        private List<APICompany> _existingCompanies;
-
-        public Menu1(
-            List<APICompany> newCompanies, 
-            List<APICompany> existingCompanies,
-            string apiconnectionstring,
-            string token,
-            string username,
-            string password)
-        {
-            this._companiesToUpdate = new List<APICompany>();
-            this._newCompanies = new List<APICompany>();
-            this._existingCompanies = new List<APICompany>();
-            
-            this._newCompanies = newCompanies;
-            this._existingCompanies = existingCompanies;
+        private List<APICompany> _newCompaniesFromStream;
+        private List<APICompany> _companiesFromStreamAlreadyInDB;
+              
+        public Menu1(string apiconnectionstring, string token, string datastreamURL)
+        {            
             this._apiConnectionString = apiconnectionstring;
-            this._token = token;
-            this._username = username;
-            this._password = password;
-
-            this._authService = new AuthService(this._apiConnectionString);
+            this._datastreamUrl = datastreamURL;
+            this._token = token;           
+           
             this._apiService = new ApiService(this._apiConnectionString);
+            this._dataStreamService = new DataStreamService(this._apiConnectionString);
+            this._logic = new Logic();
+
+            this._newCompaniesFromStream = new List<APICompany>();
+            this._companiesFromStreamAlreadyInDB = new List<APICompany>();
+
+
         }
 
-        public ResultObject RunMenu1()
+        public void RunMenu1()
         {
             Console.Clear();
-            Console.WriteLine($"There are NewCompanies: {this._newCompanies.Count}");
-            Console.WriteLine($"There are ExistingCompanies: {this._existingCompanies.Count}");
+            Console.WriteLine($"There are New Companies From Stream: {this._newCompaniesFromStream.Count}");
+            Console.WriteLine($"There are, already existing in DB Companies From Stream: {this._companiesFromStreamAlreadyInDB.Count}");
             Console.WriteLine("");
-
-            var resultobject = new ResultObject();
-            var selectResult = new SelectMenuResultObject();
+            
             var done = false;
             while (!done)
             {              
@@ -60,55 +53,80 @@ namespace Inn2PowerDataStreamUpdater.Menues
                 switch (input)
                 {
                     case "1":
-                        resultobject = UpdateAndCreateCompanies();
+                        UpdateAndCreateCompanies();
                         break;
-                    case "2":
-                        
-                        resultobject = CreateCompanies(this._newCompanies);
+                    case "2":                        
+                        CreateCompanies(this._newCompaniesFromStream);
                         break;
                     case "3":
-                        resultobject = UpdateCompanies(this._existingCompanies);
+                        UpdateCompanies(this._companiesFromStreamAlreadyInDB);
                         break;
                     case "4":
-                        var selectmenuCreate = new SelectMenu(this._newCompanies);
+                        var selectmenuCreate = new SelectMenu(this._newCompaniesFromStream);
                         var result = selectmenuCreate.RunMenu();
-                        this._newCompanies = result.Companies;
-                        resultobject = CreateCompanies((List<APICompany>) result.SelectedCompanies);
+                        this._newCompaniesFromStream = result.Companies;
+                        CreateCompanies((List<APICompany>) result.SelectedCompanies);
                         break;
                     case "5":
-                        var selectmenuUpdate = new SelectMenu(this._existingCompanies);
+                        var selectmenuUpdate = new SelectMenu(this._companiesFromStreamAlreadyInDB);
                         var result2 = selectmenuUpdate.RunMenu();
-                        this._existingCompanies = result2.Companies;
-                        resultobject = UpdateCompanies((List<APICompany>) result2.SelectedCompanies);
+                        this._companiesFromStreamAlreadyInDB = result2.Companies;
+                        UpdateCompanies((List<APICompany>) result2.SelectedCompanies);
                         break;
                     case "e":
                         done = true;
                         break;
                     default:
                         break;
-                }
-                
-                if (!done)
+                }                
+            } 
+            
+            Console.Clear();
+            Console.WriteLine("Press Enter to exit Applikation.");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Uses Prep method placed in logic to prepare data from stream.
+        /// </summary>
+        private void PrepData()
+        {
+            var dataStreamResult = this._dataStreamService.GetDataFromStream().Result;
+            var apiCompanyResult = this._apiService.GetApiCompanies(this._token).Result;
+            var categoriesResult = this._apiService.GetSupplyChainCategorys(this._token).Result;
+            var roleResult = this._apiService.GetSupplyChainRoles(this._token).Result;
+
+            if (dataStreamResult.IsSuccesFull && apiCompanyResult.IsSuccesFull && categoriesResult.IsSuccesFull &&
+                roleResult.IsSuccesFull)
+            {
+                var prepResult = this._logic.PrepareCompanies(
+                                                            (List<DataStreamCompany>) dataStreamResult.Payload,
+                                                            (List<APICompany>) apiCompanyResult.Payload,
+                                                            (List<SupplyChainRole>) roleResult.Payload,
+                                                            (List<SupplyChainCategory>) categoriesResult.Payload);
+
+                if (prepResult.IsSuccesFull)
                 {
-                    if (resultobject.IsSuccesFull)
-                    {
-                        Console.WriteLine("The chosen operation was a succes.");
-                        Console.WriteLine("Press ENTER to Continiue");
-                        Console.ReadLine();
-                    }
-                    else
-                    {
-                        Console.WriteLine(resultobject.ErrorMessage);
-                        Console.WriteLine("Please restart applikation.");
-                        Console.WriteLine("Press ENTER to Continiue");
-                        Console.ReadLine();
-                    }
-                }               
-                
+                    var result = (ListsSubResult)prepResult.Payload;
+                    this._newCompaniesFromStream = result.NewCompanies;
+                    this._companiesFromStreamAlreadyInDB = result.ExistingCompanies;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Something went wrong preparing data from Stream and API.");
+                    Console.WriteLine("Please restart Application.");
+                    Console.ReadLine();
+                }
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Something went wrong getting data from Api and Stream.");
+                Console.WriteLine("Please restart Application.");
+                Console.ReadLine();
             }
 
-            resultobject.Payload = this._token;
-            return resultobject;
         }
         
 
@@ -116,30 +134,20 @@ namespace Inn2PowerDataStreamUpdater.Menues
         /// Combines the update and create method herrein.
         /// </summary>
         /// <returns>ResultObject</returns>
-        private ResultObject UpdateAndCreateCompanies()
+        private bool UpdateAndCreateCompanies()
         {
             var badResult = new ResultObject();
             var succesresult = new ResultObject();
-
-            var result = UpdateCompanies(this._existingCompanies);
-            if (result.IsSuccesFull == false)
+             
+            if (!UpdateCompanies(this._companiesFromStreamAlreadyInDB))
             {
-                badResult.IsSuccesFull = false;
-                badResult.ErrorMessage = "Could not Update companies.";                
-                return badResult;
-            }
-
-            var result2 = CreateCompanies(this._newCompanies);
-            if (result2.IsSuccesFull == false)
+                return false;
+            }           
+            if (!CreateCompanies(this._newCompaniesFromStream))
             {
-                badResult.IsSuccesFull = false;
-                badResult.ErrorMessage = "Could not Create companies.";
-                return badResult;
-            }
-
-            succesresult.IsSuccesFull = true;
-            succesresult.Payload = this._token;
-            return succesresult;
+                return false;
+            }            
+            return true;
         }
 
         /// <summary>
@@ -148,58 +156,30 @@ namespace Inn2PowerDataStreamUpdater.Menues
         /// </summary>
         /// <param name="existingCompanies">Companies to update</param>
         /// <returns>ResultObject</returns>
-        private ResultObject UpdateCompanies(List<APICompany> existingCompanies)
-        {
-            var badResult = new ResultObject();
-            var succesresult = new ResultObject();
+        private bool UpdateCompanies(List<APICompany> existingCompanies)
+        {            
             if (!existingCompanies.Any())
             {
-                succesresult.IsSuccesFull = true;
-                return succesresult;
-            }
-            var done = false;
-            var tries = 0;
-            while (!done)
+                Console.WriteLine("Companies to Update is EMPTY. Press enter:");
+                Console.ReadLine();
+                return false;
+            }            
+
+            var result = this._apiService.UpdateCompanies(this._token, existingCompanies).Result;
+            if (result.IsSuccesFull == true)
             {
-                if (tries == 1)
-                {
-                    done = true;
-                   
-                    badResult.IsSuccesFull = false;
-                    badResult.ErrorMessage = "Tries reached. Could not login.";
-                    return badResult;
-                }
-
-                var result = this._apiService.UpdateCompanies(this._token, existingCompanies).Result;
-                if (result.IsSuccesFull == true)
-                {
-                    done = true;
-                    
-                    succesresult.IsSuccesFull = true;
-                    succesresult.Payload = result.Payload;
-                    return succesresult;
-                }
-                else
-                {
-                    if (!ReLogin())
-                    {
-                        done = true;
-
-                        
-                        badResult.IsSuccesFull = false;
-                        badResult.ErrorMessage = "Could not login";
-                        return badResult;
-                    }
-
-                    tries++;
-                }
+                Console.WriteLine("Companies have been Updated. Press Enter:");
+                Console.ReadLine();
+                return true;
             }
-            //placed to saticfy return at all path condition eventhough unreachable
-
-            badResult.IsSuccesFull = false;
-            badResult.ErrorMessage = "Unreachable is reached. This shuold not be posible";
-            return badResult;
-        }
+            else
+            {
+                Console.WriteLine("Companies COULD NOT be Updated. Press Enter:");
+                Console.ReadLine();
+                return false;
+            }
+        }            
+        
 
         /// <summary>
         /// Tries to create companies into DB via ApiService.
@@ -207,82 +187,37 @@ namespace Inn2PowerDataStreamUpdater.Menues
         /// </summary>
         /// <param name="newCompanies">New companies</param>
         /// <returns>ResultObject</returns>
-        private ResultObject CreateCompanies(List<APICompany> newCompanies)
-        {
-            var badResult = new ResultObject();
-            var succesresult = new ResultObject();
+        private bool CreateCompanies(List<APICompany> newCompanies)
+        {           
             if (!newCompanies.Any())
             {
-                succesresult.IsSuccesFull = true;
-                return succesresult;
+                Console.WriteLine("Companies to Create is EMPTY. Press enter:");
+                Console.ReadLine();
+                return false;
             }
             
-            var done = false;
-            var tries = 0;
-            while (!done)
+            var result = this._apiService.CreateCompanies(this._token, newCompanies).Result;
+            if (result.IsSuccesFull == true)
             {
-                if (tries == 1)
-                {
-                    done = true;
-                
-                    badResult.IsSuccesFull = false;
-                    badResult.ErrorMessage = "Tries reached. Could not login.";
-                    return badResult;
-                }
-
-                var result = this._apiService.CreateCompanies(this._token, newCompanies).Result;
-                if (result.IsSuccesFull == true)
-                {
-                    done = true;
-
-                    succesresult.IsSuccesFull = true;
-                    succesresult.Payload = this._token;
-                    return succesresult;
-                }
-                else
-                {
-                    if (!ReLogin())
-                    {
-                        done = true;
-
-                        badResult.IsSuccesFull = false;
-                        badResult.ErrorMessage = "Could not login";
-                        return badResult;
-                    }
-
-                    tries++;
-                }
-            }
-            //placed to saticfy return at all path condition eventhough unreachable
-
-            badResult.IsSuccesFull = false;
-            badResult.ErrorMessage = "Unreachable is reached. This shuold not be posible";
-            return badResult;
-        }
-
-        /// <summary>
-        /// Tries to ReeLogin in case of expired token.
-        /// </summary>
-        /// <returns>true or false</returns>
-        private bool ReLogin()
-        {
-            var result = this._authService.Login(this._username, this._password).Result;
-            if (result.IsSuccesFull)
-            {
-                this._token = result.Payload.ToString();
+                Console.WriteLine("Companies have been Created. Press Enter:");
+                Console.ReadLine();
                 return true;
             }
             else
             {
+                Console.WriteLine("Companies COULD NOT be Created. Press Enter:");
+                Console.ReadLine();
                 return false;
             }
-        }
+        }           
+       
+       
 
         private void Menu1Dialog()
         {
             Console.Clear();
-            Console.WriteLine($"New Companies: {this._newCompanies.Count}");
-            Console.WriteLine($"ExistingCompanies: {this._existingCompanies.Count}");
+            Console.WriteLine($"New Companies: {this._newCompaniesFromStream.Count}");
+            Console.WriteLine($"ExistingCompanies: {this._companiesFromStreamAlreadyInDB.Count}");
             Console.WriteLine("");
             Console.WriteLine("Select an option by typen its NUMBER.");
             Console.WriteLine("");
